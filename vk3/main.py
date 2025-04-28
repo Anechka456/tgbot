835ca1fa98f8f3a1f24d0bef701478f19d2e4d76
Анечка <Ann4455666@yandex.ru>
2025-04-28 12:14:19 +0300

    Вики-бот

--- vk3/main.py
+++ vk3/main.py
@@ -1,11 +1,29 @@
 import datetime
 import os
 
+import requests
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
 
 BOT_TOKEN = 'vk1.a.fiJ9tNTQbXgCsZWBA8j8tg4HWULupOb1k5we06kAWbJIHy3EzhG6dHnCz3DtN2n1vUewIMGEnaJNRrVW3bf-9EIkUQUEe__hIdasO-hAFCwfiFuxGXWVIVUYut8UXrOShaZ0jueEw0ojM8ZcWIgMxCCUL6TouCwFLStRR8ndXDToA6zt8JO0eCdIs-T12f1h1Mm9mBF8fpG59I9K036Pkg'
+
+
+def get_wikipedia_text(title, lang='ru'):
+    url = f"https://{lang}.wikipedia.org/api/rest_v1/page/summary/{title}"
+    headers = {'Accept': 'application/json; charset=utf-8'}
+
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            return data.get('extract', 'Текст статьи не найден')
+        else:
+            return f"Ошибка: {response.status_code} - {response.reason}"
+    except Exception as e:
+        return f"Ошибка запроса: {str(e)}"
+
+
 def main():
     vk_session = vk_api.VkApi(
         token=BOT_TOKEN)
@@ -21,20 +39,19 @@ def main():
             vk = vk_session.get_api()
             response = vk.users.get(user_id=event.obj.message['from_id'], fields='city')[0]
             print(response)
+            print(event.obj.message)
             txt = event.obj.message['text'].lower()
-            if 'время' in txt or 'число' in txt or 'дата' in \
-                    txt or 'день' in txt:
+            if 'найди' in txt:
+                response_viki = get_wikipedia_text(txt.split('найди')[-1].strip())
+                vk.messages.send(user_id=event.obj.message['from_id'],
+                                 message=response_viki,
+                                 random_id=random.randint(0, 64 * 2))
+            else:
                 vk.messages.send(user_id=event.obj.message['from_id'],
                                  message=f"Привет, {response['first_name']}!",
                                  random_id=random.randint(0, 64 * 2))
                 vk.messages.send(user_id=event.obj.message['from_id'],
-                                 message=f"Дата: {datetime.datetime.now().date()}, время: {datetime.datetime.now().time()}, "
-                                         f"день недели: {datetime.datetime.now().today().weekday()}",
-                                 random_id=random.randint(0, 64 * 2))
-            else:
-                vk.messages.send(user_id=event.obj.message['from_id'],
-                                 message=f"Привет, {response['first_name']}!\n"
-                                         f"Напиши мне слово число, время, дата или день, а я выведу информацию о дне",
+                                 message=f"Напиши команду найди 'запрос' и я найду это информацию в вики",
                                  random_id=random.randint(0, 64 * 2))
 
 if __name__ == '__main__':
